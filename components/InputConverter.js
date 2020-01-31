a31154f621ef5e2f937abb0d774030b4492bbaf0
julianheckmann <julianheckmann@gmail.com>
2020-01-31 17:51:55 +0100

    InputConverter.js and InputConverter-test.js

---
+++ components/InputConverter.js
@@ -0,0 +1,97 @@
+import {TYPE_DOESNT_EXISTS} from "../constants/Errors";
+
+/**
+ * Converter Class
+ */
+export default class InputConverter {
+
+    /**
+     * Converter Constructor to setup the types.
+     */
+    constructor() {
+        this.types = {
+            left: {
+                shorthand: "l",
+            },
+            right: {
+                shorthand: "r"
+            },
+            start: {
+                shorthand: "s",
+                value: 0,
+                x: {
+                    symbol: "+"
+                },
+                y: {
+                    symbol: "+"
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param type
+     * @param x
+     * @param y
+     * @returns {*}
+     */
+    convert(type, x = null, y = null) {
+        switch (type) {
+            case "start":
+                const typeInformation = this.types[type];
+                return this._buildString(typeInformation.shorthand, typeInformation.x.symbol, typeInformation.y.symbol, typeInformation.value);
+            case "left":
+            case "right":
+                if (x === null || y === null) {
+                    throw "X or Y is not set!"
+                }
+
+                return this._buildString(this.types[type].shorthand, this._plusOrMinus(x), this._plusOrMinus(y), this._calculate(x, y));
+            default:
+                throw TYPE_DOESNT_EXISTS(type);
+        }
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @returns {number}
+     * @private
+     */
+    _calculate = (x, y) => {
+        if (x < 0) {
+            x *= -1;
+        }
+
+        if (y < 0) {
+            y *= -1;
+        }
+
+        return ((Number(x).toFixed(3) * 1000) * 1024) + (Number(y).toFixed(3) * 1000);
+    };
+
+    /**
+     *
+     * @param type
+     * @param symbolX
+     * @param symbolY
+     * @param value
+     * @returns {string}
+     * @private
+     */
+    _buildString = (type, symbolX, symbolY, value) => {
+        return `${type}|${symbolX}|${symbolY}|${value}`;
+    };
+
+    /**
+     *
+     * @param axis
+     * @returns {string}
+     * @private
+     */
+    _plusOrMinus = axis => {
+        return Number(axis) > 0 ? "+" : "-";
+    };
+}
--- constants/Errors.js
+++ constants/Errors.js
@@ -2,3 +2,5 @@ export const WEBSOCKET_NO_CONNECTIONS = {
   slug: "ws_no_connection",
   displayText: "Es konnte keine Verbindung zum Server hergestellt werden."
 };
+
+export const TYPE_DOESNT_EXISTS = type => `The type ${type} doesn't exists!`;
