b8211f38949b21a9631b1193ed332bcfb4d2bdd5
julianheckmann <julianheckmann@gmail.com>
2020-01-31 20:57:20 +0100

    Screen refactoring, Implementation of the start Button, Websocket Error Handling and restructuring

--- screens/ControlScreen.js
+++ screens/ControlScreen.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import {View, StyleSheet, StatusBar} from 'react-native';
+import {View, StyleSheet, StatusBar, TouchableOpacity, Text} from 'react-native';
 import {TouchEventDemuxer, JoystickDemuxed} from 'joystick-component-lib';
 import {ScreenOrientation} from 'expo';
-import {WEBSOCKET_SERVER_ADDRESS} from '../constants/Configuration';
-import {HOMESCREEN} from '../constants/Navigations';
-import {WEBSOCKET_NO_CONNECTIONS} from '../constants/Errors';
-
-import ws from '../components/ws';
+import InputConverter from "../components/InputConverter";
 
 const Demuxer = TouchEventDemuxer([JoystickDemuxed, JoystickDemuxed]);
 
@@ -14,125 +10,157 @@ const Demuxer = TouchEventDemuxer([JoystickDemuxed, JoystickDemuxed]);
  * Control Screen Component
  */
 class ControlScreen extends React.Component {
-  /**
-   * Navigation Options
-   * @type {{headerShown: boolean}}
-   */
-  static navigationOptions = {
-    headerShown: false,
-  };
 
-  /**
-   * Sending the Data to the Websocket
-   * @param controllerSide
-   * @param x
-   * @param y
-   */
-  sendToWs = (controllerSide, x, y) => {
-    console.log(`${controllerSide[0]}${x > 0 ? '+' : '-'}${y > 0 ?
-        '+' :
-        '-'}${(x.toFixed(3).split('.')[1] * 1024) +
-    +y.toFixed(3).split('.')[1]}`);
-  };
+    constructor(props) {
+        super(props);
+        this.converter = new InputConverter();
+    }
 
-  /**
-   * Right Joystick Handler
-   * @param x
-   * @param y
-   */
-  rightJoyStickHandler = (x, y) => {
-    this.sendToWs('right', x, y);
-  };
+    /**
+     * Navigation Options
+     * @type {{headerShown: boolean}}
+     */
+    static navigationOptions = {
+        headerShown: false,
+    };
 
-  /**
-   * Left Joystick Handler
-   * @param x
-   * @param y
-   */
-  leftJoyStickHandler = (x, y) => {
-    this.sendToWs('left', x, y);
-  };
+    /**
+     * Sending the Data to the Websocket
+     * @param controllerSide
+     * @param x
+     * @param y
+     */
+    sendToWs = (controllerSide, x = null, y = null) => {
+        global.ws.send(this.converter.convert(controllerSide, x, y));
+    };
 
-  /**
-   * After mounting react hook to set the Orientation async to Landscape and
-   * instantiate the ws wrapper class
-   * @returns {Promise<void>}
-   */
-  async componentDidMount() {
-    await ScreenOrientation.lockAsync(
-        ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
-  }
+    /**
+     * Right Joystick Handler
+     * @param x
+     * @param y
+     */
+    rightJoyStickHandler = (x, y) => {
+        this.sendToWs('right', x, y);
+    };
 
-  redirectToHomeScreen = async (reason, error = null) => {
-    this.props.navigation.navigate(HOMESCREEN, {reason, error});
-  };
+    /**
+     * Left Joystick Handler
+     * @param x
+     * @param y
+     */
+    leftJoyStickHandler = (x, y) => {
+        this.sendToWs('left', x, y);
+    };
 
-  /**
-   * Content of the ControlScreen
-   * @returns {*}
-   */
-  render() {
-    return (
-        <View>
-          <Demuxer
-              childrenProps={[
-                {
-                  neutralPointX: 100,
-                  neutralPointY: 300,
-                  length: 75,
-                  shape: 'circular',
-                  isSticky: true,
-                  onJoystickMove: this.leftJoyStickHandler,
-                  draggableStyle: styles.draggableStyle,
-                  backgroundStyle: styles.circularBackgroundStyle,
-                },
-                {
-                  neutralPointX: 720,
-                  neutralPointY: 300,
-                  length: 75,
-                  shape: 'circular',
-                  isSticky: true,
-                  onJoystickMove: this.rightJoyStickHandler,
-                  draggableStyle: styles.draggableStyle,
-                  backgroundStyle: styles.circularBackgroundStyle,
-                },
-              ]}
-          />
-        </View>
-    );
-  }
+    /**
+     * Start Button Handler
+     */
+    startHandler = () => {
+        this.sendToWs('start');
+    };
+
+    /**
+     * After mounting react hook to set the Orientation async to Landscape and
+     * instantiate the ws wrapper class
+     * @returns {Promise<void>}
+     */
+    async componentDidMount() {
+        await ScreenOrientation.lockAsync(
+            ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+    }
+
+    /**
+     * Content of the ControlScreen
+     * @returns {*}
+     */
+    render() {
+        return (
+            <View>
+                <TouchableOpacity
+                    onPress={this.startHandler}
+                    style={styles.startButtonContainer}
+                >
+                    <Text style={styles.startButton}>
+                        Start
+                    </Text>
+                </TouchableOpacity>
+                <Demuxer
+                    childrenProps={[
+                        {
+                            neutralPointX: 100,
+                            neutralPointY: 300,
+                            length: 75,
+                            shape: 'circular',
+                            isSticky: true,
+                            onJoystickMove: this.leftJoyStickHandler,
+                            draggableStyle: styles.draggableStyle,
+                            backgroundStyle: styles.circularBackgroundStyle,
+                        },
+                        {
+                            neutralPointX: 720,
+                            neutralPointY: 300,
+                            length: 75,
+                            shape: 'circular',
+                            isSticky: true,
+                            onJoystickMove: this.rightJoyStickHandler,
+                            draggableStyle: styles.draggableStyle,
+                            backgroundStyle: styles.circularBackgroundStyle,
+                        },
+                    ]}
+                />
+            </View>
+        );
+    }
 }
 
 /**
  * Styling of the Control Screen
  */
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: StatusBar.currentHeight,
-    paddingHorizontal: 20,
-    height: 50,
-    width: 50,
-    backgroundColor: 'green',
-  },
-  draggableStyle: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'rgba(0,0,0, 0.8)',
-  },
-  backgroundStyle: {
-    backgroundColor: '#FFFFFF',
-    borderColor: '#D12668',
-    borderWidth: 3,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderBottomRightRadius: 20,
-    borderBottomLeftRadius: 20,
-  },
-  circularBackgroundStyle: {
-    backgroundColor: 'rgba(0,0,0, 0.1)',
-    borderColor: 'rgba(0,0,0, 0.5)',
-    borderWidth: 2,
-  },
+    container: {
+        paddingTop: StatusBar.currentHeight,
+        paddingHorizontal: 20,
+        height: 50,
+        width: 50,
+        backgroundColor: 'green',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    draggableStyle: {
+        height: 100,
+        width: 100,
+        backgroundColor: 'rgba(0,0,0, 0.8)',
+    },
+    backgroundStyle: {
+        backgroundColor: '#FFFFFF',
+        borderColor: '#D12668',
+        borderWidth: 3,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomRightRadius: 20,
+        borderBottomLeftRadius: 20,
+    },
+    circularBackgroundStyle: {
+        backgroundColor: 'rgba(0,0,0, 0.1)',
+        borderColor: 'rgba(0,0,0, 0.5)',
+        borderWidth: 2,
+    },
+    startButtonContainer: {
+        position: "absolute",
+        top: StatusBar.currentHeight - 10,
+        width: "100%",
+        flex: 1,
+        alignItems: "center"
+    },
+    startButton: {
+        width: "30%",
+        backgroundColor: "rgba(40,167,69,1)",
+        padding: 15,
+        borderRadius: 10,
+        textAlign: "center",
+        color: "white"
+    }
 });
 
 export default ControlScreen;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,204 +1,242 @@
 import React from 'react';
 import {
-  ScrollView,
-  StyleSheet,
-  StatusBar,
-  Text,
-  TouchableOpacity,
-  View,
+    ScrollView,
+    StyleSheet,
+    StatusBar,
+    Text,
+    TouchableOpacity,
+    View,
 } from 'react-native';
 
 import Layout from '../constants/Layout';
 import Colors from '../constants/Colors';
 import {ScreenOrientation} from 'expo';
-import {CONTROLSCREEN} from '../constants/Navigations';
+import {CONTROLSCREEN, HOMESCREEN} from '../constants/Navigations';
 import {WEBSOCKET_SERVER_ADDRESS} from '../constants/Configuration';
 import {WEBSOCKET_NO_CONNECTIONS} from '../constants/Errors';
-import isDisabled
-  from 'react-native-web/dist/modules/AccessibilityUtil/isDisabled';
 
 /**
  * Home Screen Component
  */
 class HomeScreen extends React.Component {
-  /**
-   * Navigation Options
-   * @type {{headerShown: boolean}}
-   */
-  static navigationOptions = {
-    headerShown: false,
-  };
 
-  state = {
-    test: "hey",
-    connectButton: {
-      isDisabled: true,
-      buttonText: 'Connect',
-      text: {
-        disabled: 'Retry',
-        enabled: 'Connect',
-        info: 'Verbindung konnte nicht hergstellt werden.',
-      },
-    },
-  };
-
-  changePrimaryButton = () => {
-
-  };
-
-  changeInfoContainer = () => {
-
-  };
-
-  /**
-   * Button event to redirect to the control component and
-   * connect to the Websocket Server
-   */
-  connectToServer = () => {
-    console.log("hey");
-
-    console.log(this.state.test);
-    // this.state.connectButton.isDisabled = !this.state.connectButton.isDisabled;
-    // this.state.connectButton.buttonText = this.state.connectButton.isDisabled ?
-    //     this.state.connectButton.text.disabled :
-    //     this.state.connectButton.text.enabled;
-    // console.log(this.state.connectButton);
-    // global.ws = new WebSocket(WEBSOCKET_SERVER_ADDRESS);
-    //
-    // /**
-    //  * On connection event
-    //  */
-    // global.ws.onopen = async () => {
-    //   await global.ws.send('app connected').then(() => {
-    //     this.props.navigation.navigate(CONTROLSCREEN);
-    //   });
-    // };
-    //
-    // /**
-    //  * On message event
-    //  * @param e
-    //  */
-    // global.ws.onmessage = e => {
-    //   console.log(e.data);
-    // };
-    //
-    // /**
-    //  * On error event
-    //  * @param e
-    //  */
-    // global.ws.onerror = e => {
-    //   console.log(e);
-    // };
-    //
-    // /**
-    //  * On close event
-    //  * @param e
-    //  */
-    // global.ws.onclose = e => {
-    //   console.log(e.message);
-    // };
-  };
-
-  /**
-   * After mounting react hook to set the Orientation to Portrait
-   */
-  componentDidMount() {
-    if (this.props) {
-      console.log(this.props);
+    /**
+     *
+     * @param props
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            companyName: "bytive",
+            connectButtonIsEnabled: true,
+            connectButtonTextEnabled: "Connect",
+            connectButtonTextDisabled: "Try again",
+            infoContainerText: WEBSOCKET_NO_CONNECTIONS.displayText
+        }
     }
 
-    this.props.navigation.addListener('willFocus', async () => {
-      await ScreenOrientation.lockAsync(
-          ScreenOrientation.OrientationLock.PORTRAIT);
-    });
-  }
+    /**
+     * Navigation Options
+     * @type {{headerShown: boolean}}
+     */
+    static navigationOptions = {
+        headerShown: false,
+    };
 
-  /**
-   * Content of the HomeScreen
-   * @returns {*}
-   */
-  render() {
-    return (
-        <View style={styles.container}>
-          <ScrollView contentContainerStyle={styles.contentContainer}>
-            <View style={styles.logoContainer}>
-              <Text style={styles.logo}>bytive</Text>
-            </View>
+    /**
+     * Button event to redirect to the control component and
+     * connect to the Websocket Server
+     */
+    connectToServer = () => {
+        if (!global.ws || global.ws.readyState === 0) {
+            global.ws = new WebSocket(WEBSOCKET_SERVER_ADDRESS);
 
-            <View style={styles.connectionButtonContainer}>
-              <TouchableOpacity
-                  onPress={this.connectToServer}
-                  style={styles.connectionButton}
-              >
-                <Text
-                    style={styles.connectionButtonText}>
-                  {this.state.test}
-                </Text>
-              </TouchableOpacity>
+            /**
+             * On connection event
+             */
+            global.ws.onopen = () => {
+                global.ws.send('app connected');
+                this.setState({
+                    connectButtonIsEnabled: true
+                });
+                this.props.navigation.navigate(CONTROLSCREEN);
+            };
+
+            /**
+             * On message event
+             * @param e
+             */
+            global.ws.onmessage = e => {
+                console.log(e.data);
+            };
+
+            /**
+             * On error event
+             * @param e
+             */
+            global.ws.onerror = e => {
+                delete global.ws;
+                this.setState({
+                    connectButtonIsEnabled: false
+                });
+                console.log(e);
+                this.props.navigation.navigate(HOMESCREEN);
+            };
+
+            /**
+             * On close event
+             * @param e
+             */
+            global.ws.onclose = e => {
+                delete global.ws;
+                this.setState({
+                    connectButtonIsEnabled: false
+                });
+                console.log(e.message);
+                this.props.navigation.navigate(HOMESCREEN);
+            };
+        } else if (global.ws && global.ws.readyState === 1) {
+            this.props.navigation.navigate(CONTROLSCREEN)
+        }
+
+    };
+
+    /**
+     * After mounting react hook to set the Orientation to Portrait
+     */
+    componentDidMount() {
+        this.props.navigation.addListener('willFocus', async () => {
+            await ScreenOrientation.lockAsync(
+                ScreenOrientation.OrientationLock.PORTRAIT);
+        });
+    }
+
+    handleChange = () => {
+        this.setState({label: "qwe"});
+    };
+
+    /**
+     * Content of the HomeScreen
+     * @returns {*}
+     */
+    render() {
+        const {companyName, connectButtonIsEnabled, connectButtonTextEnabled, connectButtonTextDisabled, infoContainerText} = this.state;
+
+        const InfoContainer = props => {
+            const {isActive, infoContainerText} = props;
+
+            if (!isActive) {
+                return (
+                    <View style={styles.notificationContainer}>
+                        <Text
+                            style={styles.notificationContainerText}>
+                            {infoContainerText}
+                        </Text>
+                    </View>
+                )
+            }
+
+            return null
+        };
+
+        return (
+            <View style={styles.container}>
+                <ScrollView contentContainerStyle={styles.contentContainer}>
+                    <View style={styles.logoContainer}>
+                        <Text style={styles.logo}>
+                            {companyName}
+                        </Text>
+                    </View>
+
+                    <View style={styles.connectionButtonContainer}>
+                        <TouchableOpacity
+                            onPress={this.connectToServer}
+                            style={connectButtonIsEnabled ? styles.connectionButton : styles.connectButtonDisabled}
+                        >
+                            <Text
+                                style={styles.connectionButtonText}>
+                                {connectButtonIsEnabled ? connectButtonTextEnabled : connectButtonTextDisabled}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+
+                <InfoContainer isActive={connectButtonIsEnabled} infoContainerText={infoContainerText}/>
             </View>
-            <View>
-              <Text
-                  style={styles.infoContainerText}>
-                {this.state.connectButton.isDisabled ?
-                    this.state.connectButton.text.info :
-                    ''}</Text>
-            </View>
-          </ScrollView>
-        </View>
-    );
-  }
+        );
+    }
 }
 
 /**
  * Styling of the HomeScreen
  */
 const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingTop: StatusBar.currentHeight,
-      },
-      contentContainer: {},
-      logoContainer: {
-        alignItems: 'center',
-        marginHorizontal: 50,
-        marginTop: Layout.window.height / 5,
-      },
-      logo: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        lineHeight: 24,
-        textAlign: 'center',
-      },
-      connectionButtonContainer: {
-        marginTop: Layout.window.height / 6,
-        alignItems: 'center',
-      },
-      connectionButton: {
-        marginTop: 25,
-        marginHorizontal: 50,
-        backgroundColor: Colors.tintColor,
-        padding: 15,
-        alignSelf: 'stretch',
-        borderRadius: 100,
-      },
-      connectionButtonText: {
-        textAlign: 'center',
-        color: '#fff',
-      },
-      infoContainerText: {
-        marginTop: 25,
-        marginHorizontal: 80,
-        backgroundColor: 'rgba(0,0,0,0.1)',
-        color: 'rgba(0,0,0,0.7)',
-        padding: 10,
-        alignSelf: 'stretch',
-        borderRadius: 25,
-        borderWidth: 1,
-        borderColor: 'rgba(0,0,0,0.2)',
-        textAlign: 'center',
-        fontSize: 9,
-      },
+        connectButtonDisabled: {
+            marginTop: 25,
+            marginHorizontal: 50,
+            backgroundColor: "rgba(36,36,36,0.5)",
+            padding: 15,
+            alignSelf: 'stretch',
+            borderRadius: 100,
+        },
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            paddingTop: StatusBar.currentHeight,
+        },
+        contentContainer: {},
+        logoContainer: {
+            alignItems: 'center',
+            marginHorizontal: 50,
+            marginTop: Layout.window.height / 5,
+        },
+        logo: {
+            fontSize: 17,
+            color: 'rgba(96,100,109, 1)',
+            lineHeight: 24,
+            textAlign: 'center',
+        },
+        connectionButtonContainer: {
+            marginTop: Layout.window.height / 6,
+            alignItems: 'center',
+        },
+        notificationContainer: {
+            position: "absolute",
+            bottom: 0,
+            width: "100%",
+            alignItems: 'center',
+            backgroundColor: "red",
+        },
+        notificationContainerText: {
+            paddingVertical: 20,
+            width: "75%",
+            alignItems: 'center',
+        },
+        connectionButton: {
+            marginTop: 25,
+            marginHorizontal: 50,
+            backgroundColor: Colors.tintColor,
+            padding: 15,
+            alignSelf: 'stretch',
+            borderRadius: 100,
+        },
+        connectionButtonText: {
+            textAlign: 'center',
+            color: '#fff',
+        },
+        infoContainerText: {
+            marginTop: 25,
+            marginHorizontal: 80,
+            backgroundColor: 'rgba(0,0,0,0.1)',
+            color: 'rgba(0,0,0,0.7)',
+            padding: 10,
+            alignSelf: 'stretch',
+            borderRadius: 25,
+            borderWidth: 1,
+            borderColor: 'rgba(0,0,0,0.2)',
+            textAlign: 'center',
+            fontSize: 9,
+        },
     })
 ;
 
